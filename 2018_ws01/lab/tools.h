b29515a56a1a1c69832ebaccf99feb782029fd7f
Chungon <cotse900@gmail.com>
2022-05-13 10:47:21 -0400

    Add files via upload

---
+++ 2018_ws01/lab/tools.h
@@ -0,0 +1,10 @@
+//May 2022 Chungon Tse
+#ifndef TOOLS_H
+#define TOOLS_H
+
+// Performs a fool-proof integer entry
+int getInt(int min, int max);
+// Displays the user interface menu
+int menu();
+
+#endif //! TOOLS_H
