4db9066e1850a6d8e8098871aa5199f7622ead74
Chungon <cotse900@gmail.com>
2022-07-29 19:25:10 -0400

    Create ex35_money.cpp

---
+++ udemy_cpp_ex/ex35_money.cpp
@@ -0,0 +1,44 @@
+#ifndef _MONEY_MONEY_H
+#define _MONEY_MONEY_H
+
+
+#include <iostream>
+
+class Money {
+    int dollars;
+    int cents;
+public:
+    Money(int dollars, int cents);
+    Money(int total);
+    int get_dollars() const {return dollars;}
+    int get_cents() const {return cents; }
+    
+
+    //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+    //----WRITE YOUR METHOD DECLARATIONS BELOW THIS LINE----
+  friend Money operator+(const Money &left, const Money &right);
+    //----WRITE YOUR METHOD DECLARATIONS ABOVE THIS LINE----
+    //----DO NOT MODIFY THE CODE BELOW THIS LINE----
+};
+
+
+#endif //_MONEY_MONEY_H
+
+
+#include "Money.h"
+
+Money::Money(int dollars, int cents) : dollars{dollars}, cents{cents} {}
+
+Money::Money(int total) : dollars {total/100}, cents{total%100}  {}
+   
+
+//----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+//----WRITE YOUR METHOD DEFINITIONS BELOW THIS LINE----
+Money operator+(const Money &left, const Money &right){
+  int total = (left.dollars + right.dollars) * 100;
+  total += left.cents + right.cents;
+  return total;
+};
+//----WRITE YOUR METHOD DEFINITIONS ABOVE THIS LINE----
+
+
